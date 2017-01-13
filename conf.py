e0052843db6a9df0ebbe35344c5c6b7a5b812f38
mlefort <m.lefort@neogeo-online.net>
2017-01-13 10:08:25 +0100

    Fix accent sur titre projet Géofoncier

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Géofoncier'
+project = 'G&eacute;ofoncier'
 copyright = '2017, Geofoncier SAS'
 author = 'Geofoncier SAS'
 
