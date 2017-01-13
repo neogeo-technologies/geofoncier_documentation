c9bca0261ffb1f213e09ae18cb1c8105224afcac
mlefort <m.lefort@neogeo-online.net>
2017-01-13 10:03:50 +0100

    Maj CSS pour élargir la zone de texte

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'geofoncier'
+project = 'Géofoncier'
 copyright = '2017, Geofoncier SAS'
 author = 'Geofoncier SAS'
 
@@ -126,10 +126,23 @@ todo_include_todos = False
 import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if not on_rtd:  # only import and set the theme if we're building docs locally   
+    import sphinx_rtd_theme                                                      
+    html_theme = 'sphinx_rtd_theme'                                              
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]                   
+    # Override default css to get a larger width for local build                 
+    def setup(app):                                                              
+        #app.add_javascript("custom.js")                                         
+        app.add_stylesheet('theme_overrides.css')                                
+else:                                                                            
+    # Override default css to get a larger width for ReadTheDoc build            
+    html_context = {                                                             
+        'css_files': [                                                           
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',       
+            '_static/theme_overrides.css',                                       
+        ],                                                                       
+    }
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
